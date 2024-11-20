477231fde86346e2cfe03ea98fb6192d5d893f67
shaowen <shaowen310@outlook.com>
2024-11-20 12:47:01 +0000

    zeroshot scripts

--- PatchTST_supervised/run.sh
+++ PatchTST_supervised/run.sh
@@ -4,3 +4,8 @@ bash scripts/PatchTST/etth1.sh
 bash scripts/PatchTST/etth2.sh
 bash scripts/PatchTST/ettm1.sh
 bash scripts/PatchTST/ettm2.sh
+
+# bash scripts/PatchTST/zeroshot/etth1_etth2.sh
+# bash scripts/PatchTST/zeroshot/etth2_etth1.sh
+# bash scripts/PatchTST/zeroshot/ettm1_ettm2.sh
+# bash scripts/PatchTST/zeroshot/ettm2_ettm1.sh
